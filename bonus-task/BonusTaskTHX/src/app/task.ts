a0f2837745728b85514c3068275bda95d5d06bfc
yanekissayabully <156779209+yanekissayabully@users.noreply.github.com>
2025-06-03 20:48:07 +0500

    Delete bonus-task/BonusTaskTHX directory

--- bonus-task/BonusTaskTHX/src/app/task.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-    id: number;
-  title: string;
-  description: string;
-  category: 'Work' | 'Personal' | 'Study';
-  status: 'Pending' | 'Completed';
-  deadline: Date;
-}
